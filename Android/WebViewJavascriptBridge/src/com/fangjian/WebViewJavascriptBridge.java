2d7e4c3219c5670a11602290ea37cfd59ec823b1
Marc Cenac <mcenac@boundlessgeo.com>
2015-10-22 14:53:08 -0500

    removing uneeded code for spatialconnect-js

--- Android/WebViewJavascriptBridge/src/com/fangjian/WebViewJavascriptBridge.java
+++ Android/WebViewJavascriptBridge/src/com/fangjian/WebViewJavascriptBridge.java
@@ -1,43 +1,40 @@
 package com.fangjian;
+
 import android.app.Activity;
-import android.content.Context;
 import android.util.Log;
-import android.webkit.*;
+import android.webkit.ConsoleMessage;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
+
 import org.json.JSONObject;
 
-import com.example.WebViewJavascriptBridgeExample.R;
-
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/**
- * Created with IntelliJ IDEA.
- * implements Serializable in case of javascript interface will be removed in obfuscated code.
- * 
- * User: jack_fang
- * Date: 13-8-15
- * Time: 下午6:08
- */
 public class WebViewJavascriptBridge implements Serializable {
 
     WebView mWebView;
     Activity mContext;
     WVJBHandler _messageHandler;
-    Map<String,WVJBHandler> _messageHandlers;
-    Map<String,WVJBResponseCallback> _responseCallbacks;
+    Map<String, WVJBHandler> _messageHandlers;
+    Map<String, WVJBResponseCallback> _responseCallbacks;
     long _uniqueId;
 
-    public WebViewJavascriptBridge(Activity context,WebView webview,WVJBHandler handler) {
-        this.mContext=context;
-        this.mWebView=webview;
-        this._messageHandler=handler;
-        _messageHandlers=new HashMap<String,WVJBHandler>();
-        _responseCallbacks=new HashMap<String, WVJBResponseCallback>();
-        _uniqueId=0;
+    public WebViewJavascriptBridge(Activity context, WebView webview, WVJBHandler handler) {
+        this.mContext = context;
+        this.mWebView = webview;
+        this._messageHandler = handler;
+        _messageHandlers = new HashMap<String, WVJBHandler>();
+        _responseCallbacks = new HashMap<String, WVJBResponseCallback>();
+        _uniqueId = 0;
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.addJavascriptInterface(this, "_WebViewJavascriptBridge");
@@ -45,19 +42,11 @@ public class WebViewJavascriptBridge implements Serializable {
         mWebView.setWebChromeClient(new MyWebChromeClient());     //optional, for show console and alert
     }
 
-
-    private void loadWebViewJavascriptBridgeJs(WebView webView) {
-        InputStream is=mContext.getResources().openRawResource(R.raw.webviewjavascriptbridge);
-        String script=convertStreamToString(is);
-        webView.loadUrl("javascript:"+script);
-
-    }
-
     public static String convertStreamToString(java.io.InputStream is) {
-        String s="";
-        try{
+        String s = "";
+        try {
             Scanner scanner = new Scanner(is, "UTF-8").useDelimiter("\\A");
-            if (scanner.hasNext()) s= scanner.next();
+            if (scanner.hasNext()) s = scanner.next();
             is.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -69,7 +58,6 @@ public class WebViewJavascriptBridge implements Serializable {
         @Override
         public void onPageFinished(WebView webView, String url) {
             Log.d("test", "onPageFinished");
-            loadWebViewJavascriptBridgeJs(webView);
         }
     }
 
@@ -77,7 +65,7 @@ public class WebViewJavascriptBridge implements Serializable {
         @Override
         public boolean onConsoleMessage(ConsoleMessage cm) {
             Log.d("test", cm.message()
-                    +" line:"+ cm.lineNumber()
+                            + " line:" + cm.lineNumber()
             );
             return true;
         }
@@ -93,124 +81,122 @@ public class WebViewJavascriptBridge implements Serializable {
         }
     }
 
-    public interface WVJBHandler{
-        public void handle(String data,WVJBResponseCallback jsCallback);
+    public interface WVJBHandler {
+        public void handle(String data, WVJBResponseCallback jsCallback);
     }
 
-    public interface WVJBResponseCallback{
+    public interface WVJBResponseCallback {
         public void callback(String data);
     }
 
-    public void registerHandler(String handlerName,WVJBHandler handler) {
-        _messageHandlers.put(handlerName,handler);
+    public void registerHandler(String handlerName, WVJBHandler handler) {
+        _messageHandlers.put(handlerName, handler);
     }
 
-    private class CallbackJs implements WVJBResponseCallback{
+    private class CallbackJs implements WVJBResponseCallback {
         private final String callbackIdJs;
 
-        public  CallbackJs(String callbackIdJs){
-            this.callbackIdJs=callbackIdJs;
+        public CallbackJs(String callbackIdJs) {
+            this.callbackIdJs = callbackIdJs;
         }
+
         @Override
         public void callback(String data) {
-               _callbackJs(callbackIdJs,data);
+            _callbackJs(callbackIdJs, data);
         }
     }
 
 
-    private void _callbackJs(String callbackIdJs,String data) {
+    private void _callbackJs(String callbackIdJs, String data) {
         //TODO: CALL js to call back;
-        Map<String,String> message=new HashMap<String, String>();
-        message.put("responseId",callbackIdJs);
-        message.put("responseData",data);
+        Map<String, String> message = new HashMap<String, String>();
+        message.put("responseId", callbackIdJs);
+        message.put("responseData", data);
         _dispatchMessage(message);
     }
 
     @JavascriptInterface
-    public void _handleMessageFromJs(String data,String responseId,
-                                     String responseData,String callbackId,String handlerName){
-        if (null!=responseId) {
+    public void _handleMessageFromJs(final String data, String responseId,
+                                     String responseData, String callbackId, String handlerName) {
+        if (null != responseId) {
             WVJBResponseCallback responseCallback = _responseCallbacks.get(responseId);
             responseCallback.callback(responseData);
             _responseCallbacks.remove(responseId);
         } else {
-            WVJBResponseCallback responseCallback = null;
-            if (null!=callbackId) {
-                responseCallback=new CallbackJs(callbackId);
-            }
-            WVJBHandler handler;
-            if (null!=handlerName) {
+            final WVJBResponseCallback responseCallback = (null != callbackId) ? new CallbackJs(callbackId) : null;
+            final WVJBHandler handler;
+            if (null != handlerName) {
                 handler = _messageHandlers.get(handlerName);
-                if (null==handler) {
-                    Log.e("test","WVJB Warning: No handler for "+handlerName);
-                    return ;
+                if (null == handler) {
+                    Log.e("test", "WVJB Warning: No handler for " + handlerName);
+                    return;
                 }
             } else {
                 handler = _messageHandler;
             }
             try {
-                mContext.runOnUiThread(new Runnable(){
+                mContext.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         handler.handle(data, responseCallback);
                     }
                 });
-            }catch (Exception exception) {
-                Log.e("test","WebViewJavascriptBridge: WARNING: java handler threw. "+exception.getMessage());
+            } catch (Exception exception) {
+                Log.e("test", "WebViewJavascriptBridge: WARNING: java handler threw. " + exception.getMessage());
             }
         }
     }
 
     public void send(String data) {
-        send(data,null);
+        send(data, null);
     }
 
-    public void send(String data ,WVJBResponseCallback responseCallback) {
-        _sendData(data,responseCallback,null);
+    public void send(String data, WVJBResponseCallback responseCallback) {
+        _sendData(data, responseCallback, null);
     }
 
-    private void _sendData(String data,WVJBResponseCallback responseCallback,String  handlerName){
-        Map <String, String> message=new HashMap<String,String>();
-        message.put("data",data);
-        if (null!=responseCallback) {
-            String callbackId = "java_cb_"+ (++_uniqueId);
-            _responseCallbacks.put(callbackId,responseCallback);
-            message.put("callbackId",callbackId);
+    private void _sendData(String data, WVJBResponseCallback responseCallback, String handlerName) {
+        Map<String, String> message = new HashMap<String, String>();
+        message.put("data", data);
+        if (null != responseCallback) {
+            String callbackId = "java_cb_" + (++_uniqueId);
+            _responseCallbacks.put(callbackId, responseCallback);
+            message.put("callbackId", callbackId);
         }
-        if (null!=handlerName) {
+        if (null != handlerName) {
             message.put("handlerName", handlerName);
         }
         _dispatchMessage(message);
     }
 
-    private void _dispatchMessage(Map <String, String> message){
+    private void _dispatchMessage(Map<String, String> message) {
         String messageJSON = new JSONObject(message).toString();
-        Log.d("test","sending:"+messageJSON);
-       final  String javascriptCommand =
-                String.format("javascript:WebViewJavascriptBridge._handleMessageFromJava('%s');",doubleEscapeString(messageJSON));   
-        mContext.runOnUiThread(new Runnable(){
+        Log.d("test", "sending:" + messageJSON);
+        final String javascriptCommand =
+                String.format("javascript:WebViewJavascriptBridge._handleMessageFromJava('%s');", doubleEscapeString(messageJSON));
+        mContext.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mWebView.loadUrl(javascriptCommand);    
+                mWebView.loadUrl(javascriptCommand);
             }
         });
     }
 
 
-    public  void callHandler(String handlerName) {
+    public void callHandler(String handlerName) {
         callHandler(handlerName, null, null);
     }
 
-    public void callHandler(String handlerName,String data) {
-        callHandler(handlerName, data,null);
+    public void callHandler(String handlerName, String data) {
+        callHandler(handlerName, data, null);
     }
 
-    public void callHandler(String handlerName,String data,WVJBResponseCallback responseCallback){
-        _sendData(data, responseCallback,handlerName);
+    public void callHandler(String handlerName, String data, WVJBResponseCallback responseCallback) {
+        _sendData(data, responseCallback, handlerName);
     }
 
     /*
-      * you must escape the char \ and  char ", or you will not recevie a correct json object in 
+      * you must escape the char \ and  char ", or you will not recevie a correct json object in
       * your javascript which will cause a exception in chrome.
       *
       * please check this and you will know why.
@@ -218,14 +204,14 @@ public class WebViewJavascriptBridge implements Serializable {
       * http://www.json.org/
     */
     private String doubleEscapeString(String javascript) {
-      String result;
-      result = javascript.replace("\\", "\\\\");
-      result = result.replace("\"", "\\\"");
-      result = result.replace("\'", "\\\'");
-      result = result.replace("\n", "\\n");
-      result = result.replace("\r", "\\r");
-      result = result.replace("\f", "\\f");
-     return result;
+        String result;
+        result = javascript.replace("\\", "\\\\");
+        result = result.replace("\"", "\\\"");
+        result = result.replace("\'", "\\\'");
+        result = result.replace("\n", "\\n");
+        result = result.replace("\r", "\\r");
+        result = result.replace("\f", "\\f");
+        return result;
     }
 
 }
